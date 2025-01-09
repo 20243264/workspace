4c549c04245774772848ffc717b0e33501f12655
김예진 <kimwisegem@gmail.com>
2025-01-09 11:21:05 +0900

    Lecture 00, 06, 99

---
+++ ch06_loop/06_for_problem3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//별찍기
+int main() {
+    for(int i=1;i<=5;i++) { 
+        for(int j=1;j<=i;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("##########\n");
+    for(int i=5;i>=1;i--) { 
+        for(int j=1;j<=i;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ ch06_loop/09_break_continue.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+*break:즉시 반복문을 빠져 나가세요 
+*continue:즉시 다음 반복으로 넘어가세요 
+*/
+int main() {
+    for(int i=1;i<=10;i++) {
+        
+    }
+
+}
