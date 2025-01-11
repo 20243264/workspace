89f8d7fa9e6c2daa44b0a233aa608fc492ec0aa3
20243264 <kimwisegem@gmail.com>
2025-01-11 17:33:29 +0900

    Update 01_simple_sort.c

--- ch04_operator/ch99_homework/01_simple_sort.c
+++ ch04_operator/ch99_homework/01_simple_sort.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-//3개의 수를 입력받고, 큰 숫자로 정렬해서 출력하는 프로그램
 int main() {
-    int num1=20, num2=10, num3=50;
-    printf("%d>%d%>%d",num1,num2,num3);
+    int num1, num2, num3, temp;
+    
+    // 사용자로부터 세 개의 수 입력받기
+    printf("세 개의 수를 입력하세요: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+    // 세 수를 내림차순으로 정렬하기
+    if (num1 < num2) { temp = num1; num1 = num2; num2 = temp; }
+    if (num1 < num3) { temp = num1; num1 = num3; num3 = temp; }
+    if (num2 < num3) { temp = num2; num2 = num3; num3 = temp; }
+    
+    // 정렬된 결과 출력
+    printf("정렬된 결과: %d > %d > %d\n", num1, num2, num3);
+    
+    return 0;
 }
