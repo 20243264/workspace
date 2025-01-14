93be646f5bad6a5fadff63d97e2114d319fddb96
김예진 <kimwisegem@gmail.com>
2025-01-14 11:49:20 +0900

    Lecture 08

--- ch07_method/ch08_array/06_gets_puts.c
+++ ch07_method/ch08_array/06_gets_puts.c
@@ -5,7 +5,7 @@ int main() {
     //출력:문자열 입력(puts)
     char name[20];
     printf("이름:");
-    gets(name);
+    gets(name); //빈칸을 포함하는 문자열 입력 가능
     puts("입력된 문자열:");
-    puts(name);
+    puts(name); //자동으로 +"\n" 포함, 문자열만 출력 가능
 }
